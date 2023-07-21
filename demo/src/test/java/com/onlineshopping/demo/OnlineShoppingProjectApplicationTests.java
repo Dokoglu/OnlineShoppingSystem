c91c5cdddd5a1f579548241a4f5507a6daa5a4f8
senavatansever <117160782+senavatansever@users.noreply.github.com>
2023-07-21 21:14:54 +0300

    Opened maven project

---
+++ demo/src/test/java/com/onlineshopping/demo/OnlineShoppingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlineshopping.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShoppingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
