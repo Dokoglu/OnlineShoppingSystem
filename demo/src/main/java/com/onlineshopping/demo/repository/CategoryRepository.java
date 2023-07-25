a11da7a4abd660738bac92150f56cb737426ed6a
Kadir Tavlı <101571967+Dokoglu@users.noreply.github.com>
2023-07-25 14:22:08 +0300

    Update CategoryRepository.java

--- demo/src/main/java/com/onlineshopping/demo/repository/CategoryRepository.java
+++ demo/src/main/java/com/onlineshopping/demo/repository/CategoryRepository.java
@@ -1,5 +1,10 @@
 package com.onlineshopping.demo.repository;
 
-public interface CategoryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.onlineshopping.demo.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category,Integer > {
+
+	
 }
