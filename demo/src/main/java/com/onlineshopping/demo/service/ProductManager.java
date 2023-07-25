61b06c6e6986a7be16d558a7a985f1284d53ea7d
senavatansever <117160782+senavatansever@users.noreply.github.com>
2023-07-25 16:34:37 +0300

    Update ProductManager Class

--- demo/src/main/java/com/onlineshopping/demo/service/ProductManager.java
+++ demo/src/main/java/com/onlineshopping/demo/service/ProductManager.java
@@ -1,5 +1,26 @@
 package com.onlineshopping.demo.service;
 
-public class ProductManager {
+import java.util.ArrayList;
 
+import com.onlineshopping.demo.entities.Product;
+
+public class ProductManager implements ProductService{
+	
+	ArrayList<Product> productList = new ArrayList<>();
+	
+	public void add(Product product) {		
+		productList.add(product);
+	}
+	
+	public void delete(Product product) { //Direkt olarak id almamalı produnc.getID yapmalıyız
+		productList.remove(product);
+	}
+	
+	public void update(Product product) {
+		for(Product p: productList) {
+			if(p.getProductID() == product.getProductID())
+				p = product;
+		}
+	}
+	
 }
