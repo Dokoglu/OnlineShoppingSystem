27e2601917ff439af4f6f0e05c98ce67c03c8453
senavatansever <117160782+senavatansever@users.noreply.github.com>
2023-07-25 15:13:41 +0300

    Updated Orders Class

--- demo/src/main/java/com/onlineshopping/demo/entities/Orders.java
+++ demo/src/main/java/com/onlineshopping/demo/entities/Orders.java
@@ -1,5 +1,59 @@
 package com.onlineshopping.demo.entities;
 
-public class Orders {
+public class Orders extends Customer{
 
+	private int orderID;
+	private String orderDate;
+	private int customerID;
+	
+	
+	
+	public Orders(int id, int creditCardId, int orderID, String orderDate, int customerID) {
+		super(id, creditCardId);
+		this.orderID = orderID;
+		this.orderDate = orderDate;
+		this.customerID = customerID;
+	}
+
+
+	//Getter and Setter Methods
+	
+	public int getOrderID() {
+		return orderID;
+	}
+
+
+
+	public void setOrderID(int orderID) {
+		this.orderID = orderID;
+	}
+
+
+
+	public String getOrderDate() {
+		return orderDate;
+	}
+
+
+
+	public void setOrderDate(String orderDate) {
+		this.orderDate = orderDate;
+	}
+
+
+
+	public int getCustomerID() {
+		return customerID;
+	}
+
+
+
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+
+	
+	
+	
+	
 }
