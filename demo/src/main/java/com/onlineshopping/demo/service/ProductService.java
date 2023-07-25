782a0553f874e77c54134467fe986336a79113bf
senavatansever <117160782+senavatansever@users.noreply.github.com>
2023-07-25 16:35:49 +0300

    Update ProductService Class

--- demo/src/main/java/com/onlineshopping/demo/service/ProductService.java
+++ demo/src/main/java/com/onlineshopping/demo/service/ProductService.java
@@ -5,7 +5,7 @@ import com.onlineshopping.demo.entities.Product;
 public interface ProductService {
 
 	public void add(Product product);
-	public void delete(int id);
+	public void delete(Product product);
 	public void update(Product product);
 	
 	
