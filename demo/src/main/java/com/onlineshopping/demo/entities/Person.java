6a3641b87a0168272ce6dc394986811f1230d5e7
senavatansever <117160782+senavatansever@users.noreply.github.com>
2023-07-25 15:07:01 +0300

    Updated Person Class

--- demo/src/main/java/com/onlineshopping/demo/entities/Person.java
+++ demo/src/main/java/com/onlineshopping/demo/entities/Person.java
@@ -2,4 +2,64 @@ package com.onlineshopping.demo.entities;
 
 public class Person {
 
+	private String name;
+	private String surname;
+	private String phoneNumber;
+	private String eMail;
+	private String adress;
+	
+	public Person(String name, String surname, String phoneNumber, String eMail, String adress) {
+		
+		this.name = name;
+		this.surname = surname;
+		this.phoneNumber = phoneNumber;
+		this.eMail = eMail;
+		this.adress = adress;
+	}
+	
+	
+	
+	
+	//Getter Setter Methods
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String geteMail() {
+		return eMail;
+	}
+
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+
+	public String getAdress() {
+		return adress;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	
 }
