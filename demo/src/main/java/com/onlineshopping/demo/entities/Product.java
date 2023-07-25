d31cc7648c3f4761ddfd65194b62855dc83f656a
senavatansever <117160782+senavatansever@users.noreply.github.com>
2023-07-25 15:18:20 +0300

    Updated Product Class

--- demo/src/main/java/com/onlineshopping/demo/entities/Product.java
+++ demo/src/main/java/com/onlineshopping/demo/entities/Product.java
@@ -2,4 +2,45 @@ package com.onlineshopping.demo.entities;
 
 public class Product {
 
+	private int productID;
+	private String productName;
+	private String description;
+	private String productType;
+	public Product(int productID, String productName, String description, String productType) {
+		
+		this.productID = productID;
+		this.productName = productName;
+		this.description = description;
+		this.productType = productType;
+	}
+	
+	
+	//Getter and Setter Methods
+	
+	public int getProductID() {
+		return productID;
+	}
+	public void setProductID(int productID) {
+		this.productID = productID;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getProductType() {
+		return productType;
+	}
+	public void setProductType(String productType) {
+		this.productType = productType;
+	}
+	
+	
 }
