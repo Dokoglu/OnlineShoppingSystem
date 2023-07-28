82fb073c462784ff397b6ec4c66d314ee9536163
senavatansever <117160782+senavatansever@users.noreply.github.com>
2023-07-28 10:29:55 +0300

    Create CustomerController

---
+++ demo/src/main/java/com/onlineshopping/demo/controllers/CustomerController.java
@@ -0,0 +1,41 @@
+package com.onlineshopping.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.onlineshopping.demo.dto.CreateCustomerRequest;
+import com.onlineshopping.demo.service.CustomerService;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+	@Autowired
+	private CustomerService customerService;
+	
+	@PostMapping("/addCustomer")
+	public void add(@RequestBody CreateCustomerRequest createCustomerRequest) {
+		customerService.add(createCustomerRequest);
+	}
+	
+	@DeleteMapping("/deleteCustomer")
+	public void delete(@RequestParam int id) {
+		customerService.delete(id);
+	}
+	
+	@GetMapping("/viewCustomers")
+	public void getAll() {
+		customerService.getAll();
+	}
+	
+	@PutMapping("/update")
+	public void update(@RequestBody CreateCustomerRequest createCustomerRequest, @RequestParam int id) {
+		customerService.update(createCustomerRequest, id);
+	}
+}
