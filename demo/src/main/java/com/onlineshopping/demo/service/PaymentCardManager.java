19e688860c224f9ef2854f5bf9ec8768348c2d79
senavatansever <117160782+senavatansever@users.noreply.github.com>
2023-08-07 10:55:02 +0300

    Create PaymentCardManager.java

---
+++ demo/src/main/java/com/onlineshopping/demo/service/PaymentCardManager.java
@@ -0,0 +1,52 @@
+package com.onlineshopping.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.onlineshopping.demo.dto.CreatePaymentCardRequest;
+import com.onlineshopping.demo.entities.PaymentCard;
+import com.onlineshopping.demo.repository.PaymentCardRepository;
+
+public class PaymentCardManager implements PaymentCardService {
+
+	@Autowired
+	private PaymentCardRepository paymentCardRepository;
+
+	@Override
+	public void add(CreatePaymentCardRequest paymentCardRequest) {
+		PaymentCard paymentCard = new PaymentCard();
+
+		paymentCard.setCardNumber(paymentCardRequest.getCardNumber());
+		paymentCard.setCvc(paymentCardRequest.getCvc());
+		paymentCard.setDate(paymentCardRequest.getDate());
+		paymentCard.setNameSurname(paymentCardRequest.getNameSurname());
+
+		paymentCardRepository.save(paymentCard);
+
+	}
+
+	@Override
+	public void delete(int id) {
+		paymentCardRepository.deleteById(id);
+
+	}
+
+	@Override
+	public void update(CreatePaymentCardRequest paymentCardRequest, int id) {
+		PaymentCard paymentCard =  paymentCardRepository.findById(id).get();
+		
+		paymentCard.setCardNumber(paymentCardRequest.getCardNumber());
+		paymentCard.setCvc(paymentCardRequest.getCvc());
+		paymentCard.setDate(paymentCardRequest.getDate());
+		paymentCard.setNameSurname(paymentCardRequest.getNameSurname());
+		
+		paymentCardRepository.save(paymentCard);
+	}
+
+	@Override
+	public List<PaymentCard> getAll() {
+		return paymentCardRepository.findAll();
+	}
+
+}
