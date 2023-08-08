232b944375d75efb108ad08ac48fe598707e1d27
senavatansever <117160782+senavatansever@users.noreply.github.com>
2023-08-08 18:08:11 +0300

    Update Category Class

--- demo/src/main/java/com/onlineshopping/demo/entities/Category.java
+++ demo/src/main/java/com/onlineshopping/demo/entities/Category.java
@@ -1,12 +1,9 @@
 package com.onlineshopping.demo.entities;
-import java.util.List;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
