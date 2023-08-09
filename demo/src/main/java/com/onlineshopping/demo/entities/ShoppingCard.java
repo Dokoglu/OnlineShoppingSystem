ce534b8aed1e43e88d8c827b03a7ce77722ad740
senavatansever <117160782+senavatansever@users.noreply.github.com>
2023-08-09 13:56:32 +0300

    Update ShoppingCard.java

--- demo/src/main/java/com/onlineshopping/demo/entities/ShoppingCard.java
+++ demo/src/main/java/com/onlineshopping/demo/entities/ShoppingCard.java
@@ -1,9 +1,10 @@
 package com.onlineshopping.demo.entities;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+@Entity
 public class ShoppingCard {
 
 	@Id
