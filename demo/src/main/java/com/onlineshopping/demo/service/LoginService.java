3e0ce1e8a4b98d5b256cd0272063058eedef163b
senavatansever <117160782+senavatansever@users.noreply.github.com>
2023-08-17 22:07:17 +0300

    Update LoginService.java

--- demo/src/main/java/com/onlineshopping/demo/service/LoginService.java
+++ demo/src/main/java/com/onlineshopping/demo/service/LoginService.java
@@ -1,9 +1,11 @@
 package com.onlineshopping.demo.service;
 
+import java.util.Optional;
+
 import com.onlineshopping.demo.controllers.model.LoginBody;
 import com.onlineshopping.demo.entities.Customer;
 
 public interface LoginService {
 
-	Customer login(LoginBody loginBody);
+	Optional<Customer> login(LoginBody loginBody);
 }
