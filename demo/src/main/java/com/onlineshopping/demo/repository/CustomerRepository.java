63767b7076deff9055221502519889095eabd471
senavatansever <117160782+senavatansever@users.noreply.github.com>
2023-08-21 13:31:40 +0300

    Update CustomerRepository.java

--- demo/src/main/java/com/onlineshopping/demo/repository/CustomerRepository.java
+++ demo/src/main/java/com/onlineshopping/demo/repository/CustomerRepository.java
@@ -1,7 +1,5 @@
 package com.onlineshopping.demo.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
@@ -12,5 +10,4 @@ import com.onlineshopping.demo.entities.Customer;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer>{
 	
-	Optional <Customer> findByEmailAndPassword(String e_mail, String password);
 	}
