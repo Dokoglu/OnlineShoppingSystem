e927a7a636dbdea5ed31d534c2727f000ac9cb88
senavatansever <117160782+senavatansever@users.noreply.github.com>
2023-08-22 15:23:04 +0300

    Update Product.java

--- demo/src/main/java/com/onlineshopping/demo/entities/Product.java
+++ demo/src/main/java/com/onlineshopping/demo/entities/Product.java
@@ -1,14 +1,10 @@
 package com.onlineshopping.demo.entities;
 
-import java.util.List;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Table(name="Product")
