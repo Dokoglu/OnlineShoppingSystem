cd86f43dc3964e4295922c17db738b73b709df8d
senavatansever <117160782+senavatansever@users.noreply.github.com>
2023-08-25 12:20:53 +0300

    Update SearchingManager.java

--- demo/src/main/java/com/onlineshopping/demo/service/SearchingManager.java
+++ demo/src/main/java/com/onlineshopping/demo/service/SearchingManager.java
@@ -1,6 +1,5 @@
 package com.onlineshopping.demo.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,30 +15,7 @@ public class SearchingManager implements SearchingService {
 
 	@Override
 	public List<Product> findByWord(String word) {
-	
-		        return productService.findNameContainingIgnoreCase(word);
-		    }
+
+		return productService.findNameContainingIgnoreCase(word);
 	}
-//		List<Product> listedByWord = new ArrayList<>();
-//		
-//		for (Product  p: productService.findAll()) { //sorun burada
-//			if(p.getProductName().contains(word))
-//				listedByWord.add(p);
-//		}
-//		return listedByWord;
-//	}
-
-
- /*for (Product  p: productService.findAll()) {
-			for (int i = 0; i <= p.getProductName().length(); i++) {
-				int j;
-				for (j = 0; j < word.length(); j++) {
-					if (p.getProductName().charAt(i + j) != word.charAt(j)) {
-						break;
-					}
-				}
-				if (j == word.length()) {
-					listedByWord.add(p);
-				}
-			}
-		}*/
+}
