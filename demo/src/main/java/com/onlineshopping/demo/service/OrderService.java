e0956a732188a39e949bb99a5a114693a472671a
senavatansever <117160782+senavatansever@users.noreply.github.com>
2023-08-28 17:40:52 +0300

    Update OrderService.java

--- demo/src/main/java/com/onlineshopping/demo/service/OrderService.java
+++ demo/src/main/java/com/onlineshopping/demo/service/OrderService.java
@@ -1,9 +1,19 @@
 package com.onlineshopping.demo.service;
 
+import java.util.List;
+
+import com.onlineshopping.demo.dto.CreateOrderRequest;
 import com.onlineshopping.demo.entities.Orders;
 
 public interface OrderService {
-	
-	void addOrder(Orders orders);
 
+	void addOrder(CreateOrderRequest orderRequest);
+
+	List<Orders> getAllOrders();
+
+	List<Orders> getCustomerOrdersByCustomerID(int id);
+
+	Orders getByOrderID(int id);
+
+	void cancelOrdersById(int id);
 }
