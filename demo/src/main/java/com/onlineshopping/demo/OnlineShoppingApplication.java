60654833480a806dd7132d1807e0762636a97607
senavatansever <117160782+senavatansever@users.noreply.github.com>
2023-08-28 17:40:39 +0300

    Update OnlineShoppingApplication.java

--- demo/src/main/java/com/onlineshopping/demo/OnlineShoppingApplication.java
+++ demo/src/main/java/com/onlineshopping/demo/OnlineShoppingApplication.java
@@ -1,11 +1,16 @@
 package com.onlineshopping.demo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OnlineShoppingApplication {
-
+	@Bean
+    public ModelMapper getModelMapper() {
+        return new ModelMapper();
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineShoppingApplication.class, args);
 		
