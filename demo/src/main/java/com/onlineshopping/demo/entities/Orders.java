0f9256a1a7d2e29d95f550074e1aa08eb1654258
senavatansever <117160782+senavatansever@users.noreply.github.com>
2023-08-29 12:09:44 +0300

    Update Orders.java

--- demo/src/main/java/com/onlineshopping/demo/entities/Orders.java
+++ demo/src/main/java/com/onlineshopping/demo/entities/Orders.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Orders {
@@ -30,6 +32,9 @@ public class Orders {
 		this.productPrice = productPrice;
 	}
 	
+	@ManyToOne()
+	@JoinColumn(name = "shoppingCard_ID")
+	private ShoppingCard shoppingcard;
 
 	public Orders() {
 	}
