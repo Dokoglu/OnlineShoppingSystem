69f3609a34679921b7540d4352d829949403e1a0
senavatansever <117160782+senavatansever@users.noreply.github.com>
2023-08-29 12:09:54 +0300

    Update ShoppingCard.java

--- demo/src/main/java/com/onlineshopping/demo/entities/ShoppingCard.java
+++ demo/src/main/java/com/onlineshopping/demo/entities/ShoppingCard.java
@@ -1,9 +1,16 @@
 package com.onlineshopping.demo.entities;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 @Entity
 public class ShoppingCard {
 
@@ -17,6 +24,22 @@ public class ShoppingCard {
 		this.id = id;
 		this.productID = productID;
 	}
+	
+	@OneToOne()
+	@JoinColumn(name = "customerID")
+	private Customer customer;
+	
+	@OneToMany()
+	private List<Orders> orders;
+	
+
+	
+	@ManyToMany()
+	@JoinTable(
+			  name = "shopping_card", 
+			  joinColumns = @JoinColumn(name = "shoppingcard_id"), 
+			  inverseJoinColumns = @JoinColumn(name = "product__id"))
+	private List<Product> products;
 
 	public ShoppingCard() {
 
