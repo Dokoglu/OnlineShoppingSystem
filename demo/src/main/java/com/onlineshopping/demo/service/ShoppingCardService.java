b5d4a555e8bcdceea89ee6f6f8b3942c0dba47d9
senavatansever <117160782+senavatansever@users.noreply.github.com>
2023-08-31 12:23:24 +0300

    Update ShoppingCardService.java

--- demo/src/main/java/com/onlineshopping/demo/service/ShoppingCardService.java
+++ demo/src/main/java/com/onlineshopping/demo/service/ShoppingCardService.java
@@ -2,12 +2,14 @@ package com.onlineshopping.demo.service;
 
 import java.util.Optional;
 
+import com.onlineshopping.demo.dto.CreateShoppingCardRequest;
 import com.onlineshopping.demo.entities.ShoppingCard;
 
 public interface ShoppingCardService {
 	
-	void addProduct(int productID);
+	void addProduct(CreateShoppingCardRequest shoppingCardRequest);
 	void deleteProduct(int productID);
+	void update(CreateShoppingCardRequest shoppingCardRequest, int customerID);
 	Optional<ShoppingCard> getAll(int id);
 
 }
