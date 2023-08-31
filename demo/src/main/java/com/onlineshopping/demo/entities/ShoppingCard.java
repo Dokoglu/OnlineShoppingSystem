8dcc7b8978d65a468c55722713268e5277041075
senavatansever <117160782+senavatansever@users.noreply.github.com>
2023-08-31 17:18:56 +0300

    Update ShoppingCard.java

--- demo/src/main/java/com/onlineshopping/demo/entities/ShoppingCard.java
+++ demo/src/main/java/com/onlineshopping/demo/entities/ShoppingCard.java
@@ -1,8 +1,6 @@
 package com.onlineshopping.demo.entities;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -10,7 +8,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 @Entity
@@ -19,14 +16,12 @@ public class ShoppingCard {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	private int productID; //product obje mapple
 	@Column(insertable = false, updatable = false)
 	private int customerID;
 
-	public ShoppingCard(int id, int productID, int customerID) {
+	public ShoppingCard(int id, int customerID) {
 
 		this.id = id;
-		this.productID = productID;
 		this.customerID = customerID;
 	}
 	
@@ -37,10 +32,8 @@ public class ShoppingCard {
 	@OneToMany()
 	private List<Orders> orders;
 	
-
-	
-	@ManyToMany()
-	private Set<ShoppingCard> shoppingcardSet=new HashSet<>();
+	@OneToMany()
+	private List<Product> products;
 
 	public ShoppingCard() {
 
@@ -52,14 +45,6 @@ public class ShoppingCard {
 
 	public void setId(int id) {
 		this.id = id;
-	}
-
-	public int getProductID() {
-		return productID;
-	}
-
-	public void setProductID(int productID) {
-		this.productID = productID;
 	}
 
 	public int getCustomerID() {
