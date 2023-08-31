7bca41736a51dea6b1a80fb8123a1a4328f847fe
senavatansever <117160782+senavatansever@users.noreply.github.com>
2023-08-31 12:23:12 +0300

    Update ShoppingCard.java

--- demo/src/main/java/com/onlineshopping/demo/entities/ShoppingCard.java
+++ demo/src/main/java/com/onlineshopping/demo/entities/ShoppingCard.java
@@ -4,12 +4,12 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
@@ -19,12 +19,15 @@ public class ShoppingCard {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	private int productID;
+	private int productID; //product obje mapple
+	@Column(insertable = false, updatable = false)
+	private int customerID;
 
-	public ShoppingCard(int id, int productID) {
+	public ShoppingCard(int id, int productID, int customerID) {
 
 		this.id = id;
 		this.productID = productID;
+		this.customerID = customerID;
 	}
 	
 	@OneToOne()
@@ -59,4 +62,13 @@ public class ShoppingCard {
 		this.productID = productID;
 	}
 
+	public int getCustomerID() {
+		return customerID;
+	}
+
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+
+	
 }
